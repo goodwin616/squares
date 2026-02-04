53e4b013d0ffdfe01404bbce497f50d672473650
Blake Goodwin <blakegoodwin@xemnas.local>
2026-02-03 23:49:51 -0500

    chore: remove compiled artifacts from functions source

--- functions/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const start_game: import("firebase-functions/v2/https").CallableFunction<any, Promise<{
-    success: boolean;
-    message: string;
-}>>;
-//# sourceMappingURL=index.d.ts.map
